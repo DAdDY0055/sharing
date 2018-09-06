3d6cece3965705d7a409fa5e389289dadf1be291
野口悦也 <noguchietsuya@noguchietsuyanoMacBook-puro.local>
2018-09-06 12:32:10 +0900

    プロフィール画像機能追加, config/application.rb,11行目にcarrierwaveを動くように追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180906013343) do
+ActiveRecord::Schema.define(version: 20180906015523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20180906013343) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "icon"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
