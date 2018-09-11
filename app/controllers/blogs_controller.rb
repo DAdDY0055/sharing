51d3726553f6c618980b4ba6b3c7f3d0242832a6
etsuya-noguchi <astrix0325@gmail.com>
2018-09-12 01:51:47 +0900

    blogs_controller.rbのbefore_actionのshowを削除

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,6 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:edit, :update, :destroy]
+  before_action :set_user, only: [:edit, :update, :destroy]
   before_action :user_approval
 
 
